ac1c7b334c0b19bf74b53a981c70aaaa5c09f4a6
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-08-28 22:09:04 -0300

    Criando input para fazer a pesquisar

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Input = () => {
+  return (
+    <div>
+      <input type="search" name="pesquisar" id="pesquisar" placeholder="Digite o que deseja" />
+    </div>
+  );
+};
+
+export default Input;
